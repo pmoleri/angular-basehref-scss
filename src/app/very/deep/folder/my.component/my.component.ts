0f07d8fbf13195210a74a302f118bbd77a1e69b4
Pablo <pmoleri@infragistics.com>
2024-08-29 17:53:18 -0300

    Add MyComponent with url() references

---
+++ src/app/very/deep/folder/my.component/my.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my',
+  standalone: true,
+  imports: [],
+  templateUrl: './my.component.html',
+  styleUrl: './my.component.scss'
+})
+export class MyComponentComponent {
+
+}
